578e86a1126ebcda9f6f94176c1abe70767963f7
Ojenike Queensley <queensleyojenike@gmail.com>
2025-11-04 05:50:05 +0000

    fix: header fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,12 +11,15 @@ export default function Header() {
 
   return (
     <div className="bg-black text-white flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-[16px] h-auto sm:h-[42px] w-full py-[12px] px-[66px] text-sm text-center sm:text-left">
-      <div className="whitespace-normal break-words ">
-        New season coming! Discount 10% for all products! Checkout Now!  <span className="min-w-[90px] bg-[#383838] text-center rounded-full tabular-nums px-3 py-1">
-        {now.toLocaleTimeString()}
-      </span>
+      <div className="whitespace-normal break-words">
+        New season coming! Discount 10% for all products! Checkout Now!{" "}
+        <span
+          suppressHydrationWarning
+          className="min-w-[90px] bg-[#383838] text-center rounded-full tabular-nums px-3 py-1"
+        >
+          {now.toLocaleTimeString()}
+        </span>
       </div>
-     
     </div>
   );
 }
